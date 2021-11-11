86d3294b23a50aedf9ecc83683432807e08b79c9
ManikantaProdduturi <mani.uf90@gmail.com>
2021-11-10 20:02:29 -0600

    updated

--- routes/Watches.js
+++ routes/Watches.js
@@ -5,23 +5,3 @@ var router = express.Router();
 /* GET Watchess */ 
 router.get('/', Watches_controlers.Watches_view_all_Page ); 
 module.exports = router; 
-// Handle Watches create on POST. 
-exports.Watches_create_post = async function(req, res) { 
-  console.log(req.body) 
-  let document = new Watches(); 
-  // We are looking for a body, since POST does not have query parameters. 
-  // Even though bodies can be in many different formats, we will be picky 
-  // and require that it be a json object 
-  // {"Watches_type":"goat", "cost":12, "size":"large"} 
-  document.Watches_type = req.body.Watches_type; 
-  document.cost = req.body.cost; 
-  document.size = req.body.size; 
-  try{ 
-      let result = await document.save(); 
-      res.send(result); 
-  } 
-  catch(err){ 
-      res.status(500); 
-      res.send(`{"error": ${err}}`); 
-  }   
-}; 
