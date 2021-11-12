4496c2f9ad4a1654b3ac6a4de8c80aa208b0cf3d
ManikantaProdduturi <mani.uf90@gmail.com>
2021-11-12 10:44:15 -0600

    changes made

--- controllers/Watches.js
+++ controllers/Watches.js
@@ -68,4 +68,14 @@ exports.Watches_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
- 
+// for a specific Costume. 
+exports.Watches_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Watches.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
