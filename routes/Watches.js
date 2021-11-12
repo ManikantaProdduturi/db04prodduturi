8642410066950a8c11209ca31d984e1c45f4c95b
ManikantaProdduturi <mani.uf90@gmail.com>
2021-11-12 10:45:11 -0600

    changes made

--- routes/Watches.js
+++ routes/Watches.js
@@ -5,3 +5,7 @@ var router = express.Router();
 /* GET Watchess */ 
 router.get('/', Watches_controlers.Watches_view_all_Page ); 
 module.exports = router; 
+
+// GET request for one costume. 
+router.get('/Watches/:id', Watches_controlers.Watches_detail); 
+ 
